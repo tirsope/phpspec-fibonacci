c4652f4de4f1fc9208a6190adfb0ab647d9df1a7
Tirso peralta <tirso.peralta@gmail.com>
2016-01-06 21:08:14 -0500

    Added a tests for 0, 1, 2, 3, 4 and 12

--- spec/Tirsope/FibonacciSpec.php
+++ spec/Tirsope/FibonacciSpec.php
@@ -11,4 +11,34 @@ class FibonacciSpec extends ObjectBehavior
     {
         $this->shouldHaveType('Tirsope\Fibonacci');
     }
+
+    function it_returns_zero_for_fibonacci_of_zero()
+    {
+        $this->fibonacci(0)->shouldReturn(0);
+    }
+
+    function it_returns_one_for_fibonacci_of_one()
+    {
+        $this->fibonacci(1)->shouldReturn(1);
+    }
+
+    function it_returns_one_for_fibonacci_of_two()
+    {
+        $this->fibonacci(2)->shouldReturn(1);
+    }
+
+    function it_returns_two_for_fibonacci_of_three()
+    {
+        $this->fibonacci(3)->shouldReturn(2);
+    }
+
+    function it_returns_three_for_fibonacci_of_four()
+    {
+        $this->fibonacci(4)->shouldReturn(3);
+    }
+
+    function it_returns_144_for_fibonacci_of_twelve()
+    {
+        $this->fibonacci(12)->shouldReturn(144);
+    }
 }
--- src/Tirsope/Fibonacci.php
+++ src/Tirsope/Fibonacci.php
@@ -4,4 +4,29 @@ namespace Tirsope;
 
 class Fibonacci
 {
+    public function fibonacci($number)
+    {
+        switch($number) {
+            case 0:
+                return 0;
+                break;
+
+            case 1:
+                return 1;
+                break;
+
+            default:
+                return $this->computeFibonacciFor($number);
+                break;
+        }
+    }
+
+    /**
+     * @param $number
+     * @return int
+     */
+    public function computeFibonacciFor($number)
+    {
+        return $this->fibonacci($number - 1) + $this->fibonacci($number - 2);
+    }
 }
